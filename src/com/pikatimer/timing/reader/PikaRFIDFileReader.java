c02725aaeb6e82f55da7e1523c002e346bf67fb7
John Garner <john.garner.co@gmail.com>
2016-12-19 08:29:12 -0700

    Create an abstract 'TailingReader' class to make implementing additional readers easy
    *PikaRFIDFileReader now extends TailingReader. Most of the boilerplate logic now resides in the parent, the child just implements the 'parse' method.

--- src/com/pikatimer/timing/reader/PikaRFIDFileReader.java
+++ src/com/pikatimer/timing/reader/PikaRFIDFileReader.java
@@ -18,231 +18,26 @@ package com.pikatimer.timing.reader;
 
 import com.pikatimer.event.Event;
 import com.pikatimer.timing.RawTimeData;
-import com.pikatimer.timing.TimingListener;
 import com.pikatimer.timing.TimingReader;
 import com.pikatimer.util.DurationFormatter;
 import com.pikatimer.util.DurationParser;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Platform;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Task;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressIndicator;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
-
-import org.apache.commons.io.input.Tailer;
-import org.apache.commons.io.input.TailerListenerAdapter;
-import org.controlsfx.control.ToggleSwitch;
 
 /**
  *
  * @author jcgarner
  */
-public class PikaRFIDFileReader implements TimingReader{
+public class PikaRFIDFileReader extends TailingReader implements TimingReader{
     
-    private TimingListener timingListener;
-    private File sourceFile; 
-    private final StringProperty fileName; 
-    private Pane displayPane; 
-    private Button inputButton;
-    private TextField inputTextField; 
-    private Label statusLabel; 
-    private HBox displayHBox; 
-    private VBox displayVBox; 
-    private Tailer tailer;
-    private Thread thread; 
-    private final BooleanProperty readingStatus;
-    ProgressIndicator watchProgressIndicator;
-    ToggleButton watchToggleButton;
-    ToggleSwitch autoImportToggleSwitch;
     
-    public PikaRFIDFileReader(){
-        fileName = new SimpleStringProperty();
-        readingStatus = new SimpleBooleanProperty();
-        
-    }
-
-
-
-    public void selectInput() {
-        final FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Select File");
-        
-        if (sourceFile != null) {
-            fileChooser.setInitialDirectory(sourceFile.getParentFile()); 
-            fileChooser.setInitialFileName(sourceFile.getName());
-        } else {
-            fileChooser.setInitialDirectory(new File(System.getProperty("user.home"))); 
-        }
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
-                new FileChooser.ExtensionFilter("All files", "*")
-            );
-        
-        sourceFile = fileChooser.showOpenDialog(inputButton.getScene().getWindow());
-        if (sourceFile != null) {
-            // if we are auto-importing, stop that
-            readingStatus.set(false);
-            
-            fileName.setValue(sourceFile.getAbsolutePath());
-            // save the filename 
-            timingListener.setAttribute("RFIDFileReader:filename", sourceFile.getAbsolutePath());
-            
-            // set the text field to the filename
-            inputTextField.textProperty().setValue(fileName.getValueSafe());
-            // read the file
-            if (!sourceFile.canRead()){
-                statusLabel.setText("Unable to open file: " + fileName.getValueSafe());
-            } else readOnce();
-        }                
-    }
-
     
-
-    @Override
-    public void startReading() {
-        // make sure the file exists
-        if (!sourceFile.canRead()){
-                statusLabel.setText("Unable to open file: " + fileName.getValueSafe());
-                Platform.runLater(() ->{readingStatus.set(false);});
-        } else  if (! readingStatus.getValue() ) {
-            statusLabel.setText("Reading file: " + fileName.getValueSafe());
-
-            MyHandler listener = new MyHandler();
-            tailer = Tailer.create(sourceFile, listener, 500, Boolean.FALSE, Boolean.TRUE);
-            thread = new Thread(tailer);
-            thread.setDaemon(true); // optional
-            thread.start();
-            readingStatus.setValue(Boolean.TRUE);
-        }
-    }
     
     @Override
-    public void stopReading() {
-        if (tailer != null) {
-            tailer.stop();
-        }
-        readingStatus.setValue(Boolean.FALSE);
-    }
-
-
-    @Override
-    public void showControls(Pane p) {
-        
-        if (displayPane == null) {
-            // initialize our display
-            displayHBox = new HBox();
-            displayVBox = new VBox();
-            watchProgressIndicator = new ProgressIndicator();
-            watchToggleButton = new ToggleButton("Watch File...");
-            autoImportToggleSwitch = new ToggleSwitch("Auto-Import File");
-            autoImportToggleSwitch.selectedProperty().set(false);
-            statusLabel = new Label("");
-            inputButton = new Button("Open...");
-            inputTextField = new TextField();
-            displayVBox.setSpacing(5); 
-            //displayVBox.setPadding(new Insets(5, 5, 5, 5));
-            
-            
-            inputTextField.focusedProperty().addListener((ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue) -> {
-                if (!newPropertyValue && !fileName.getValueSafe().equals(inputTextField.textProperty().getValueSafe())) {
-                    // if we are auto-importing, stop that
-                    readingStatus.set(false);
-                    
-                    sourceFile = new File(inputTextField.textProperty().getValueSafe());
-                    fileName.setValue(sourceFile.getAbsolutePath());
-
-                    
-                    // save the filename 
-                    timingListener.setAttribute("RFIDFileReader:filename", sourceFile.getAbsolutePath());
-
-                    // read the file
-                    if (!sourceFile.canRead()){
-                        statusLabel.setText("Unable to open file: " + fileName.getValueSafe());
-                    } else readOnce();
-                        
-                } else {
-                    System.out.println("No change in file name");
-                }
-            });
-            
-            displayHBox.setSpacing(5);
-            displayHBox.getChildren().addAll(inputTextField, inputButton, autoImportToggleSwitch, watchProgressIndicator); 
-            displayVBox.getChildren().addAll(displayHBox, statusLabel); 
-            
-            // Set the action for the inputButton
-            inputButton.setOnAction((event) -> {
-                // Button was clicked, do something...
-                selectInput();
-            });
-            
-            watchProgressIndicator.visibleProperty().bind(autoImportToggleSwitch.selectedProperty());
-            watchProgressIndicator.setProgress(-1.0);
-            // get the current status of the reader
-            //watchProgressIndicator.setPrefHeight(30.0);
-            watchProgressIndicator.setMaxHeight(30.0);
-            autoImportToggleSwitch.selectedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
-                if(newValue) {
-                    System.out.println("PikaRFIDFileReader: autoImportToggleSwitch event: calling startReading()");
-                    startReading();
-                } else {
-                    System.out.println("PikaRFIDFileReader: autoImportToggleSwitch event: calling stopReading()");
-                    stopReading();
-                }
-            });
-            autoImportToggleSwitch.selectedProperty().bindBidirectional(readingStatus);
-            
-            inputTextField.textProperty().setValue(fileName.getValueSafe());
-            // set the action for the inputTextField
-            
-        }
-        
-        // If we were previously visible, clear the old one
-        if (displayPane != null) displayPane.getChildren().clear();
-        
-        // Now show ourselves.... 
-        displayPane = p; 
-        displayPane.getChildren().clear();
-        displayPane.getChildren().add(displayVBox); 
-        
-        
-    }
-
-    @Override
-    public BooleanProperty getReadingStatus() {
-        return readingStatus; 
-    }
-    
-    private class MyHandler extends TailerListenerAdapter {
-
-        @Override
-        public void handle(String line) {
-            PikaRFIDFileReader.this.handle(line);
-            //System.out.println(line);
-        }
-
-    }
-    
-    
-    public void handle(String s) {
+    public void process(String s) {
         
         
         String[] tokens = s.split(",", -1);
@@ -342,56 +137,6 @@ public class PikaRFIDFileReader implements TimingReader{
 
     }
         
-    @Override
-    public void readOnce() {
-        // get the event date, just in case we need it
-        System.out.println("PikaRFIDFileReader.readOnce called. Current file is: " + sourceFile.getAbsolutePath());
-        
-        // Run this in a thread.... 
-        Task task;
-        task = new Task<Void>() {
-            @Override public Void call() {
-                try {            
-                    Files.lines(sourceFile.toPath())
-                        .map(s -> s.trim())
-                        .filter(s -> !s.isEmpty())
-                        .forEach(s -> {
-                            //System.out.println("readOnce read " + s); 
-                            handle(s); 
-                        });
-                } catch (IOException ex) {
-                    Logger.getLogger(PikaRFIDFileReader.class.getName()).log(Level.SEVERE, null, ex);
-                    // We had an issue reading the file.... 
-                }
-                return null;
-            }
-        };
-        new Thread(task).start();
-    }
     
-    
-
-    @Override
-    public void setTimingListener(TimingListener t) {
-        timingListener = t; 
-        
-        // get any existing attributes
-        String filename = timingListener.getAttribute("RFIDFileReader:filename");
-        if (filename != null) {
-            System.out.println("RFIDFileReader: Found existing file setting: " + filename);
-            sourceFile = new File(filename);
-            fileName.setValue(sourceFile.getAbsolutePath());
-            
-        } else {
-            System.out.println("RFIDFileReader: Did not find existing file setting." );
-        }
-        
-        
-    }
-    
-    @Override
-    public Boolean chipIsBib() {
-        return Boolean.FALSE; 
-    }
     
 }
---
+++ src/com/pikatimer/timing/reader/TailingReader.java
@@ -0,0 +1,290 @@
+/*
+ * Copyright (C) 2016 John Garner <segfaultcoredump@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.pikatimer.timing.reader;
+
+import com.pikatimer.timing.TimingListener;
+import com.pikatimer.timing.TimingReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import org.apache.commons.io.input.Tailer;
+import org.apache.commons.io.input.TailerListenerAdapter;
+import org.controlsfx.control.ToggleSwitch;
+
+/**
+ *
+ * @author John Garner <segfaultcoredump@gmail.com>
+ */
+public abstract class TailingReader implements TimingReader{
+        
+    protected TimingListener timingListener;
+    protected File sourceFile; 
+    protected final StringProperty fileName; 
+    private Pane displayPane; 
+    private Button inputButton;
+    protected TextField inputTextField; 
+    protected Label statusLabel; 
+    private HBox displayHBox; 
+    private VBox displayVBox; 
+    protected Tailer tailer;
+    private Thread thread; 
+    protected final BooleanProperty readingStatus;
+    ProgressIndicator watchProgressIndicator;
+    ToggleButton watchToggleButton;
+    ToggleSwitch autoImportToggleSwitch;
+    
+    public TailingReader(){
+        fileName = new SimpleStringProperty();
+        readingStatus = new SimpleBooleanProperty();
+        
+    }
+
+
+
+    public void selectInput() {
+        final FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select File");
+        
+        if (sourceFile != null) {
+            fileChooser.setInitialDirectory(sourceFile.getParentFile()); 
+            fileChooser.setInitialFileName(sourceFile.getName());
+        } else {
+            fileChooser.setInitialDirectory(new File(System.getProperty("user.home"))); 
+        }
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("All files", "*")
+            );
+        
+        sourceFile = fileChooser.showOpenDialog(inputButton.getScene().getWindow());
+        if (sourceFile != null) {
+            // if we are auto-importing, stop that
+            readingStatus.set(false);
+            
+            fileName.setValue(sourceFile.getAbsolutePath());
+            // save the filename 
+            timingListener.setAttribute("TailingReader:filename", sourceFile.getAbsolutePath());
+            
+            // set the text field to the filename
+            inputTextField.textProperty().setValue(fileName.getValueSafe());
+            // read the file
+            if (!sourceFile.canRead()){
+                statusLabel.setText("Unable to open file: " + fileName.getValueSafe());
+            } else readOnce();
+        }                
+    }
+
+    
+
+    @Override
+    public void startReading() {
+        // make sure the file exists
+        if (!sourceFile.canRead()){
+                statusLabel.setText("Unable to open file: " + fileName.getValueSafe());
+                Platform.runLater(() ->{readingStatus.set(false);});
+        } else  if (! readingStatus.getValue() ) {
+            statusLabel.setText("Reading file: " + fileName.getValueSafe());
+
+            MyHandler listener = new MyHandler();
+            tailer = Tailer.create(sourceFile, listener, 500, Boolean.FALSE, Boolean.TRUE);
+            thread = new Thread(tailer);
+            thread.setDaemon(true); // optional
+            thread.start();
+            readingStatus.setValue(Boolean.TRUE);
+        }
+    }
+    
+    @Override
+    public void stopReading() {
+        if (tailer != null) {
+            tailer.stop();
+        }
+        readingStatus.setValue(Boolean.FALSE);
+    }
+
+
+    @Override
+    public void showControls(Pane p) {
+        
+        if (displayPane == null) {
+            // initialize our display
+            displayHBox = new HBox();
+            displayVBox = new VBox();
+            watchProgressIndicator = new ProgressIndicator();
+            watchToggleButton = new ToggleButton("Watch File...");
+            autoImportToggleSwitch = new ToggleSwitch("Auto-Import File");
+            autoImportToggleSwitch.selectedProperty().set(false);
+            statusLabel = new Label("");
+            inputButton = new Button("Open...");
+            inputTextField = new TextField();
+            displayVBox.setSpacing(5); 
+            //displayVBox.setPadding(new Insets(5, 5, 5, 5));
+            
+            
+            inputTextField.focusedProperty().addListener((ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue) -> {
+                if (!newPropertyValue && !fileName.getValueSafe().equals(inputTextField.textProperty().getValueSafe())) {
+                    // if we are auto-importing, stop that
+                    readingStatus.set(false);
+                    
+                    sourceFile = new File(inputTextField.textProperty().getValueSafe());
+                    fileName.setValue(sourceFile.getAbsolutePath());
+
+                    
+                    // save the filename 
+                    timingListener.setAttribute("TailingReader:filename", sourceFile.getAbsolutePath());
+
+                    // read the file
+                    if (!sourceFile.canRead()){
+                        statusLabel.setText("Unable to open file: " + fileName.getValueSafe());
+                    } else readOnce();
+                        
+                } else {
+                    System.out.println("No change in file name");
+                }
+            });
+            
+            displayHBox.setSpacing(5);
+            displayHBox.getChildren().addAll(inputTextField, inputButton, autoImportToggleSwitch, watchProgressIndicator); 
+            displayVBox.getChildren().addAll(displayHBox, statusLabel); 
+            
+            // Set the action for the inputButton
+            inputButton.setOnAction((event) -> {
+                // Button was clicked, do something...
+                selectInput();
+            });
+            
+            watchProgressIndicator.visibleProperty().bind(autoImportToggleSwitch.selectedProperty());
+            watchProgressIndicator.setProgress(-1.0);
+            // get the current status of the reader
+            //watchProgressIndicator.setPrefHeight(30.0);
+            watchProgressIndicator.setMaxHeight(30.0);
+            autoImportToggleSwitch.selectedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+                if(newValue) {
+                    System.out.println("TailingReader: autoImportToggleSwitch event: calling startReading()");
+                    startReading();
+                } else {
+                    System.out.println("TailingReader: autoImportToggleSwitch event: calling stopReading()");
+                    stopReading();
+                }
+            });
+            autoImportToggleSwitch.selectedProperty().bindBidirectional(readingStatus);
+            
+            inputTextField.textProperty().setValue(fileName.getValueSafe());
+            // set the action for the inputTextField
+            
+        }
+        
+        // If we were previously visible, clear the old one
+        if (displayPane != null) displayPane.getChildren().clear();
+        
+        // Now show ourselves.... 
+        displayPane = p; 
+        displayPane.getChildren().clear();
+        displayPane.getChildren().add(displayVBox); 
+        
+        
+    }
+
+    @Override
+    public BooleanProperty getReadingStatus() {
+        return readingStatus; 
+    }
+    
+    private class MyHandler extends TailerListenerAdapter {
+
+        @Override
+        public void handle(String line) {
+            process(line);
+            //System.out.println(line);
+        }
+
+    }
+    
+    
+    public abstract void process(String s);
+        
+    @Override
+    public void readOnce() {
+        // get the event date, just in case we need it
+        System.out.println("TailingReader.readOnce called. Current file is: " + sourceFile.getAbsolutePath());
+        
+        // Run this in a thread.... 
+        Task task;
+        task = new Task<Void>() {
+            @Override public Void call() {
+                try {            
+                    Files.lines(sourceFile.toPath())
+                        .map(s -> s.trim())
+                        .filter(s -> !s.isEmpty())
+                        .forEach(s -> {
+                            //System.out.println("readOnce read " + s); 
+                            process(s); 
+                        });
+                } catch (IOException ex) {
+                    Logger.getLogger(TailingReader.class.getName()).log(Level.SEVERE, null, ex);
+                    // We had an issue reading the file.... 
+                }
+                return null;
+            }
+        };
+        new Thread(task).start();
+    }
+    
+    
+
+    @Override
+    public void setTimingListener(TimingListener t) {
+        timingListener = t; 
+        
+        // get any existing attributes
+        String filename = timingListener.getAttribute("TailingReader:filename");
+        if (filename != null) {
+            System.out.println("TailingReader: Found existing file setting: " + filename);
+            sourceFile = new File(filename);
+            fileName.setValue(sourceFile.getAbsolutePath());
+            
+        } else {
+            System.out.println("TailingReader: Did not find existing file setting." );
+        }
+        
+        
+    }
+    
+    @Override
+    public Boolean chipIsBib() {
+        return Boolean.FALSE; 
+    }
+
+}
