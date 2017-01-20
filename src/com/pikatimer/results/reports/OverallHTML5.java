7cbf540319556c39f44d4da3d84c213017d995e7
John Garner <john.garner.co@gmail.com>
2017-01-20 09:35:44 -0700

    Async Google Analytics script for HTML5 report

--- src/com/pikatimer/results/reports/OverallHTML5.java
+++ src/com/pikatimer/results/reports/OverallHTML5.java
@@ -121,15 +121,13 @@ public class OverallHTML5 implements RaceReportType{
         if (customHeaders && race.getStringAttribute("GACode") != null && !race.getStringAttribute("GACode").isEmpty()) {
             report +=   "<!-- Google Analytics -->\n" +
                         "<script>\n" +
-                        "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n" +
-                        "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n" +
-                        "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n" +
-                        "})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');\n" +
-                        "\n" +
+                        "window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;\n" +
                         "ga('create', '" + race.getStringAttribute("GACode") + "', 'auto');\n" +
                         "ga('send', 'pageview');\n" +
                         "</script>\n" +
-                        "<!-- End Google Analytics -->";
+                        "<script async src='https://www.google-analytics.com/analytics.js'></script>\n" +
+                        "<!-- End Google Analytics -->" ;
+                        
             report += System.lineSeparator();
         }
         
