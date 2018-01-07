d8231b5e37fbb336f11d8da7ef8f4f2f4857688b
John Garner <john.garner.co@gmail.com>
2018-01-06 17:43:46 -0700

    Better handle a brain-dead Flyway metadata table update
    * See https://github.com/flyway/flyway/issues/1248

--- src/com/pikatimer/FXMLopenEventController.java
+++ src/com/pikatimer/FXMLopenEventController.java
@@ -47,6 +47,7 @@ import javafx.stage.Stage;
 import org.apache.commons.io.FileUtils;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.MigrationInfo;
+import org.flywaydb.core.internal.dbsupport.FlywaySqlScriptException;
 import org.h2.store.fs.FilePath;
 
 /**
@@ -130,15 +131,26 @@ public class FXMLopenEventController {
                         //LoadingProgressBar.setProgress(0.2F);
 
                         // Upgrade the schema (if out of date)
+                        // Use a r/o access mode wrapped in a try/catch to look for a schema version db upgrade
+                        // since flyway modifies the database upon issuing _any_ command and would prevent us from
+                        // saving a copy that v1.0 can still read
                         try {
-                            flyway.setDataSource(jdbcURL, "sa", null);
-                            MigrationInfo[] pending = flyway.info().pending();
-                            if (pending.length > 0) {
-                                System.out.println("Pending Migrations, saving a copy");
-                                
-                                FileUtils.copyFile(dbFile, new File(dbFile.getAbsolutePath() + ".pre_v1.5_update"));
-
+                            Flyway flyway_check = new Flyway();
+                            flyway_check.setDataSource(jdbcURL + ";ACCESS_MODE_DATA=r", "sa", null);
+                            Boolean backup_needed = false;
+                            try {
+                                MigrationInfo[] pending = flyway_check.info().pending();
+                                if (pending.length > 0) backup_needed = true;
+                            } catch (FlywaySqlScriptException sql_ex){
+                                System.out.println("Pending metadata update, saving a copy");
+                                backup_needed = true;
                             }
+                            if (backup_needed) {
+                                System.out.println("Pending Migrations, saving a copy");
+                                FileUtils.copyFile(dbFile, new File(dbFile.getAbsolutePath() + ".pre_v1.5_update.pika"));
+                            }
+                            
+                            flyway.setDataSource(jdbcURL, "sa", null);
                             flyway.migrate();
                         } catch (Exception ex) {
                             ex.printStackTrace();
