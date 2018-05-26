adeca2675543b12b8ecd10720993488088d21fb1
John Garner <john.garner.co@gmail.com>
2018-05-26 15:30:15 -0600

    Prep for 1.5 release

--- src/com/pikatimer/Pikatimer.java
+++ src/com/pikatimer/Pikatimer.java
@@ -38,7 +38,7 @@ public class Pikatimer extends Application {
     private static Stage mainStage;
     private static String jdbcURL; // Holds the jdbcURL for the open db
     
-    public static final String VERSION = "1.5 Beta 1";
+    public static final String VERSION = "1.5";
     
     /**
     * SingletonHolder is loaded on the first execution of Singleton.getInstance() 
