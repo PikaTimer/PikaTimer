87260c4a2f9eb33a6063dfc2a355c729b60393df
John Garner <segfaultcoredump@gmail.com>
2024-10-08 18:13:07 -0600

    Update Copyright date

--- src/main/java/com/pikatimer/util/DurationParser.java
+++ src/main/java/com/pikatimer/util/DurationParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 John Garner <segfaultcoredump@gmail.com>
+ * Copyright (C) 2024 John Garner <segfaultcoredump@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
